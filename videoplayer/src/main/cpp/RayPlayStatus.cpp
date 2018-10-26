e3260f921aaf4c07c706fa84505885433c57e499
hikobe8@163.com <hikobe8@163.com>
2018-10-26 14:06:51 +0800

    initilize project

---
+++ videoplayer/src/main/cpp/RayPlayStatus.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Administrator on 2018/8/8.
+//
+
+#include "RayPlayStatus.h"
+
+RayPlayStatus::RayPlayStatus() {
+    this->exit = false;
+    this->isLoading = true;
+}
---
+++ videoplayer/src/main/cpp/RayPlayStatus.h
@@ -0,0 +1,19 @@
+//
+// Created by Administrator on 2018/8/8.
+//
+
+#ifndef RAYMUSIC_RAYPLAYSTATUS_H
+#define RAYMUSIC_RAYPLAYSTATUS_H
+
+
+class RayPlayStatus {
+public:
+    bool exit;
+    bool isLoading;
+    bool doSeek = false;
+public:
+    RayPlayStatus();
+};
+
+
+#endif //RAYMUSIC_RAYPLAYSTATUS_H
